cdbe45df79cf8310dc3903fa66423339ef0c61db
Snens98 <116456908+Snens98@users.noreply.github.com>
2023-05-21 13:08:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    WZoom.create('.full-height-container img', {
+
+        maxScale: 10,
+        minScale: 1,
+        speed: 5,
+        smoothExtinction: false,
+    });
+});
+
